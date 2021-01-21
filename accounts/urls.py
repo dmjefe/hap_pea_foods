e1102c60c08d287930d6f72b105f19ac661178b2
David Boss <dmboss@gmail.com>
2021-01-21 02:01:32 -0800

    Added profile model and working on the view - DB

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     url('register/', views.register_view, name="register"),
     url('login/', views.login_view, name="login"),
+    url('profile/', views.profile_view, name="profile"),
     url('logout/', views.logout_view, name="logout")
 ]
