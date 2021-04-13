c6eb68858c475f4af295e629d6b8d8a0087305b2
Suzy Kaufman <skaufma5@asu.edu>
2021-04-12 20:49:36 -0700

    sk

--- survey/forms.py
+++ survey/forms.py
@@ -3,9 +3,7 @@ from django.forms import ModelForm
 from .models import Questionnaire
 
 
-
 class QuestionnaireForm(ModelForm):
     class Meta:
         model = Questionnaire
         fields = ['question', 'option_one', 'option_two', 'option_three', 'option_four']
-    
